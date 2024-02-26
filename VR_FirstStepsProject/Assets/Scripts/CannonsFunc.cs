3f136508ee3c0d9d13fb63d08229633e7ea99c11
Blakes <dmunoz@cocoschool.mx>
2024-02-26 10:02:07 -0600

    Update Cannon
    Cannon functions

---
+++ VR_FirstStepsProject/Assets/Scripts/CannonsFunc.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class CannonsFunc : MonoBehaviour
+{
+    public GameObject socket;
+    public GameObject cannonball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
